2267abe378154505e8424c8f1c30bc7e4cd272b7
Devin Rieger <dev0089@gmail.com>
2017-02-07 23:46:35 -0500

    added exit functionality and continuous running

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,12 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+
+def runner
+  character = get_character_from_user
+  goodbye(character)
+  show_character_movies(character)
+  runner
+end
+
+runner
